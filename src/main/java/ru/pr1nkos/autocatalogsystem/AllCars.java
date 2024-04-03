ba7a828799661933631f77804574adcf6b294fff
Pr1nkos <prinkos1@gmail.com>
2024-04-03 11:25:52 +0300

    Добавил аннотацию оверайд для doGet

--- src/main/java/ru/pr1nkos/autocatalogsystem/AllCars.java
+++ src/main/java/ru/pr1nkos/autocatalogsystem/AllCars.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class AllCars extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
